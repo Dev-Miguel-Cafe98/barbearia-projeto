9e3285e5daa4b0640ca4583f1cd2b84d4a8c1551
Welley Miguel <158380657+Dev-Miguel-Cafe98@users.noreply.github.com>
2024-12-10 16:32:00 -0300

    ajustes

---
+++ contato.php
@@ -0,0 +1,32 @@
+<div id="comp-k5cws236" class="HcOXKn SxM0TO QxJLC3 lq2cno comp-k5cws236 wixui-rich-text" data-testid="richTextElement">
+        <h1 class="font_0 wixui-rich-text__text" style="font-size:62px; line-height:0.9em;"><span style="color:#222222;" class="wixui-rich-text__text"><span style="font-family:wfont_ab76ba_def3430855f6447eb5a0e3279decb8e3,wf_def3430855f6447eb5a0e3279,orig_akzidenzgroteskbecn;" class="wixui-rich-text__text"><span style="letter-spacing:0em;" class="wixui-rich-text__text"><span style="font-size:62px;" class="wixui-rich-text__text">CONTATO</span></span></span></span></h1>
+    </div>
+    <div id="comp-k5cws23b" class="bkIuWA comp-k5cws23b">
+        <div data-mesh-id="comp-k5cws23binlineContent" data-testid="inline-content" class="">
+            <div data-mesh-id="comp-k5cws23binlineContent-gridContainer" data-testid="mesh-container-content">
+                <form id="comp-kegqxf57" class="JVi7i2 comp-kegqxf57 wixui-form">
+                    <div data-mesh-id="comp-kegqxf57inlineContent" data-testid="inline-content" class="">
+                        <div data-mesh-id="comp-kegqxf57inlineContent-gridContainer" data-testid="mesh-container-content">
+                            <div id="comp-kegqxf5b" class="MpKiNN comp-kegqxf5b wixui-text-input lPl_oN LyB02C">
+                                <div class="pUnTVX"><input name="nome-*" id="input_comp-kegqxf5b" class="KvoMHf has-custom-focus wixui-text-input__input" type="text" placeholder="Nome *" required="" aria-required="true" aria-invalid="true" maxlength="100" autocomplete="off" aria-label="Nome *" value=""></div>
+                            </div>
+                            <div id="comp-kegqxf5e" class="MpKiNN comp-kegqxf5e wixui-text-input LyB02C">
+                                <div class="pUnTVX"><input name="phone" id="input_comp-kegqxf5e" class="KvoMHf has-custom-focus wixui-text-input__input" type="tel" placeholder="Celular" aria-required="false" aria-invalid="false" maxlength="50" autocomplete="off" aria-label="Celular" value=""></div>
+                            </div>
+                            <div id="comp-kegqxf5h" class="MpKiNN comp-kegqxf5h wixui-text-input lPl_oN LyB02C">
+                                <div class="pUnTVX"><input name="email" id="input_comp-kegqxf5h" class="KvoMHf has-custom-focus wixui-text-input__input" type="email" placeholder="Email *" required="" aria-required="true" aria-invalid="true" pattern="^.+@.+\.[a-zA-Z]{2,63}$" maxlength="250" autocomplete="off" aria-label="Email *" value=""></div>
+                            </div>
+                            <div id="comp-kegqxf5j2" class="MpKiNN comp-kegqxf5j2 wixui-text-input LyB02C">
+                                <div class="pUnTVX"><input name="assunto" id="input_comp-kegqxf5j2" class="KvoMHf has-custom-focus wixui-text-input__input" type="text" placeholder="Assunto" aria-required="false" aria-invalid="false" autocomplete="off" aria-label="Assunto" value=""></div>
+                            </div>
+                            <div id="comp-kegqxf5m1" class="snt4Te comp-kegqxf5m1 wixui-text-box pOExcs"><label for="textarea_comp-kegqxf5m1" class="PSkPrR wixui-text-box__label"></label><textarea id="textarea_comp-kegqxf5m1" class="rEindN has-custom-focus wixui-text-box__input" placeholder="Mensagem" aria-required="false" aria-invalid="false"></textarea></div>
+                            <div class="comp-kegqxf5p1 FubTgk" id="comp-kegqxf5p1" aria-disabled="false"><button aria-disabled="false" data-testid="buttonElement" class="uDW_Qe wixui-button PlZyDq"><span class="l7_2fn wixui-button__label">ENVIAR</span></button></div>
+                            <div id="comp-kegqxf5s1" class="HcOXKn SxM0TO QxJLC3 lq2cno comp-kegqxf5s1 wixui-rich-text" data-testid="richTextElement">
+                                <p dir="ltr" style="font-size:14px; text-align:center;" class="wixui-rich-text__text"><span style="color:#68B04D;" class="wixui-rich-text__text"><span style="font-family:helvetica-w01-roman,helvetica-w02-roman,helvetica-lt-w10-roman,sans-serif;" class="wixui-rich-text__text"><span style="font-size:14px;" class="wixui-rich-text__text">Sua mensagem foi enviada com sucesso!</span></span></span></p>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
